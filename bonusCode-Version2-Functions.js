27cd018e3f059cde90291d2676ad15af00718ddf
Candice Bianca Brown <candicebrown97@gmail.com>
2017-07-10 02:41:23 +0000

    updated draft for bonus code

--- bonusCode-Version2-Functions.js
+++ bonusCode-Version2-Functions.js
@@ -1,7 +1,14 @@
 var travelWants = ["Ghana", "Pyramids of Giza", "London", "Dominican Republic", "Japan", "Dubai", "Italy"];
 
-function changeNAdd() {
+function fullMonty() { //wrote a function that addresses all tasks - add, change, display or delete
     travelWants[4] = "Tokyo";
+    //console.log(travelWants);
+    travelWants.push("Jamaica");
+    //console.log(travelWants);
+    travelWants.splice(4, 1);
     console.log(travelWants);
 }
-changeNAdd();
+fullMonty();
+
+
+
