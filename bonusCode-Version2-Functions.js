87e6684cd6d5aa90c0dbcd2c469ad9f2d90cb639
Candice Bianca Brown <candicebrown97@gmail.com>
2017-07-10 02:17:18 +0000

    Bonus Code - V2 - Functions

---
+++ bonusCode-Version2-Functions.js
@@ -0,0 +1,7 @@
+var travelWants = ["Ghana", "Pyramids of Giza", "London", "Dominican Republic", "Japan", "Dubai", "Italy"];
+
+function changeNAdd() {
+    travelWants[4] = "Tokyo";
+    console.log(travelWants);
+}
+changeNAdd();
