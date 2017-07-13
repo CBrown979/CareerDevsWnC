b3c21a0465c850736f9871a12ffcf55f8ecdba63
Candice Bianca Brown <candicebrown97@gmail.com>
2017-07-13 09:36:56 +0000

    array Commit

---
+++ arrays.js
@@ -0,0 +1,17 @@
+//version 1 - Arrays
+var todos=["item 1", "item 2", "item 3"] //to create an array for variable named todos
+
+console.log("My todos:", todos);
+
+todos.push("item 4", "item 5"); //to add items to array
+
+console.log("My todos", todos);
+
+todos[0]="updated item 1"; //to change item 1
+
+console.log(todos);
+
+todos.splice(0,1); //to delete the first item
+
+console.log(todos);
+
